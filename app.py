4a807b0dede275c11f2e35ba48b049d65c223ea7
jalis55 <jalismahamud2055@gmail.com>
2021-05-31 11:00:04 +0600

    database migration

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import datetime
+
+
+
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:pass@localhost:5432/blood-doners"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# user model
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    email=db.Column(db.String(128))
+    mobile=db.Column(db.String(100))
+    blood_group=db.Column(db.Integer)
+
+# post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    mobile=db.Column(db.String(100))
+    blood_group=db.Column(db.Integer)
+    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
 
 
 @app.route('/')
